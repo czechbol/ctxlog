49344fc1082be78fbdc2f6dbae14dea10f839ce9
czechbol <adamludes@gmail.com>
2025-05-10 23:53:15 +0200

    Fix releases

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 project = "ctxlog"
 copyright = "2025, czechbol"
 author = "czechbol"
-release = '1.0.0'
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
